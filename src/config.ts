b6eed1d47453e45a024a590e204f5122ec965222
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-08-20 05:58:00 +0000

    import `yaml` from `esm.sh` url directly

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-import { stringify } from "yaml";
+import { stringify } from "https://esm.sh/v131/yaml@2.3.1";
 import { ConfigYaml, Workflows, WorkflowsSchema } from "./spec.ts";
 import Job from "./job.ts";
 
