4b0703659720a07b91a1f360692a14a02558518c
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-09-20 17:47:06 +0000

    use deps.ts instead of import_map.json

---
+++ deps.ts
@@ -0,0 +1,3 @@
+export { assertEquals } from "https://deno.land/std@0.191.0/testing/asserts.ts";
+export { stringify } from "https://esm.sh/v131/yaml@2.3.1";
+export { z } from "https://deno.land/x/zod@v3.22.1/mod.ts";
