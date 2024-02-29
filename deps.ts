9d96896eb7fec12f70823b9a192f94e0cebde84a
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-02-29 20:34:35 +0000

    setup jsr

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { assertEquals } from "https://deno.land/std@0.191.0/testing/asserts.ts";
-export { stringify } from "https://esm.sh/v131/yaml@2.3.1";
-export { z } from "https://deno.land/x/zod@v3.22.1/mod.ts";
+export { assertEquals } from "jsr:@std/testing@0.217.0/asserts";
+export { stringify } from "npm:yaml@2.3.1";
+export { z } from "npm:zod@3.22.1";
