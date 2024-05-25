ddd87ef1e80a8faf36d4d12eee36a920bd0e961c
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-05-25 14:36:21 +0000

    use lodash@4.17.21

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export { assertEquals } from "jsr:@std/testing@0.217.0/asserts";
 export { stringify } from "npm:yaml@2.3.1";
 export { z } from "npm:zod@3.22.1";
-import _ from "npm:lodash";
+import _ from "npm:lodash@4.17.21";
 export { _ };
